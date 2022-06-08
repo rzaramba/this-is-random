2a868b5e1a5eb48e06bd2ebd181ac4aca97136bf
Rachel Zaramba <rachel.zaramba@gmail.com>
2022-06-08 09:57:14 -0600

    fixed modal to close on click

--- script.js
+++ script.js
@@ -280,7 +280,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Add a click event on various child elements to close the parent modal
-  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button, #modalBtn') || []).forEach(($close) => {
     const $target = $close.closest('.modal');
 
     $close.addEventListener('click', () => {
