91426b16426c11302e1c15b3f553748f00df044b
vanessaroman <vanessavirginiarr@gmail.com>
2022-06-08 00:30:38 -0600

    edit results output

--- script.js
+++ script.js
@@ -100,6 +100,7 @@ function codeAddress() {
 // Yelp API
 
 // The method below using the bearer token is not safe because anyone can read the code and use the authorization for their own project, however, for the purposes of the front end of the app working and this course we are doing it like this.
+
 document.querySelector('#inputButtonGeocode').addEventListener('click', function () {
 
   var YELP_URL = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/events?locale=en_US&limit=7&city=${document.getElementById("newLocation").value}`;
@@ -136,15 +137,16 @@ document.querySelector('#inputButtonGeocode').addEventListener('click', function
     });
 
     function displayEvents(jsonData){
-
+      //loop going through all the events
       for (var i = 1; i < jsonData.events.length; i++){
+        
       // Event category
       const eventOne = jsonData.events[i];
       const eventDiv = document.querySelector('#resultsBox');
       const eventCat = eventOne.category;
       const catHeading = document.createElement('h2');
       catHeading.innerHTML = 'Category: ' + eventCat;
-      eventDiv.appendChild(catHeading);
+      eventDiv.append(catHeading);
       // Event description
       const eventDesc = eventOne.description;
       const descHeading = document.createElement('h2');
@@ -158,13 +160,80 @@ document.querySelector('#inputButtonGeocode').addEventListener('click', function
       // Event link
       const eventLink = eventOne.event_site_url;
       const linkHeading = document.createElement('h2');
-      // linkHeading.innerHTML = 'Site: ' + eventLink;
+      linkHeading.innerHTML = `<a href="${eventLink}" >Event Site</a>` + '<br><br>';
       eventDiv.appendChild(linkHeading);
     }
     }
 
 })
 
+//running same results function to show events when the find my loation button is clicked 
+
+document.querySelector('#btn').addEventListener('click', function(){
+
+  var YELP_URL = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/events?locale=en_US&limit=7&city=${document.getElementById("newLocation").value}`;
+
+  var YELP_API_KEY = '2ZhlYRdEeLQ6L6Ar-oilki-PPnB4uUxOvbLOWfrLFiJlW78u-0Cdik77mlXZKQb0nFMQ5SX8GzzgjLjNXAeTo5RjKU_KkbBskWczcaTnuwEqiP6dYgPZd_8MEbSbYnYx';
+
+
+  var req = new Request(YELP_URL, {
+    method: 'GET',
+    headers: new Headers({
+      'Authorization': `Bearer ${YELP_API_KEY}`,
+      'Content-Type': 'application/json'
+    }),
+
+  });
+
+  fetch(req)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+
+        throw new Error();
+      }
+    })
+    .then((jsonData) => {
+      console.log(jsonData);
+      displayEvents(jsonData)
+
+    })
+
+    .catch((err) => {
+      console.log('ERROR: ', err.message);
+    });
+
+    function displayEvents(jsonData){
+      //loop going through all the events
+      for (var i = 1; i < jsonData.events.length; i++){
+        
+      // Event category
+      const eventOne = jsonData.events[i];
+      const eventDiv = document.querySelector('#resultsBox');
+      const eventCat = eventOne.category;
+      const catHeading = document.createElement('h2');
+      catHeading.innerHTML = 'Category: ' + eventCat;
+      eventDiv.append(catHeading);
+      // Event description
+      const eventDesc = eventOne.description;
+      const descHeading = document.createElement('h2');
+      descHeading.innerHTML = 'Description: ' + eventDesc;
+      eventDiv.appendChild(descHeading);
+      // Event time
+      const eventTime = eventOne.time_start;
+      const timeHeading = document.createElement('h2');
+      timeHeading.innerHTML = 'Time: ' + eventTime;
+      eventDiv.appendChild(timeHeading);
+      // Event link
+      const eventLink = eventOne.event_site_url;
+      const linkHeading = document.createElement('h2');
+      linkHeading.innerHTML = `<a href="${eventLink}" >Event Site</a>` + '<br><br>';
+      eventDiv.appendChild(linkHeading);
+    }
+    }
+
+})
 
 
 
